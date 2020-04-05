72d88d91fc63fc0cce1f06ff8a5f9e7b63a76a43
Rishikesh <rishikesh1419@gmail.com>
2020-04-05 20:42:42 +0530

    To-Do List done

---
+++ HomeServer/index.js
@@ -0,0 +1,10 @@
+var express = require("express");
+var app = express();
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", function(req, res){
+	res.render("index.ejs");
+});
+
+app.listen(8080);
